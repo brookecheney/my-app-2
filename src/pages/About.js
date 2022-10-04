072ff2e2c396a430c4d0c1f7b15625876bc3b979
Brooklyne Cheney <brookecheney@hotmail.com>
2022-10-04 15:42:26 -0600

    abouty

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ImageWithText from "../components/ImageWithText";
-import myImage from "./blackbow.png";
-import { Box, Grid, Flex } from "@chakra-ui/react";
+import myImage from "./blackbow.png";import { Box, Grid, Flex } from "@chakra-ui/react";
 const About = () => {
   return (
     <Box p="2rem">
@@ -9,15 +8,11 @@ const About = () => {
         <Flex justifyContent="center" alignItems="center">
           <ImageWithText
             image={myImage}
-            heading="About"
-            text="The hair bow was originally gender-specific to adult males in Europe throughout the 1700s when men adorned their hair with bows to show they were prosperous and extravagant.
-
-            Women also wore extravagant hairstyles, but these did not often feature hair bows; rather large ornaments and jewels were preferred.
+            heading="App Privacy Policy"
+            text="https://app.termly.io/document/privacy-policy/1f637ff0-abb2-4d5a-88fe-fbce5a18235b"
             
-            After the French Revolution extravagance in dress and hairstyle was frowned upon and hair bows were rarely worn. By the 1800s it became common for male children to wear hair bows tying hair at the nape of the neck.
-            
-            Women throughout the 19th century wore hair ornaments and hats, but the hair bow only really achieved widespread popularity in the 20th century before the second world war"
           />
+          
         </Flex>
       </Grid>
     </Box>
